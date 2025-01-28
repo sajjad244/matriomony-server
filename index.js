a93d1a28d200a7594a8f6a8c484263080ab6a843
sajjad244 <mrsajjed@gmail.com>
2025-01-28 17:26:10 +0600

    complite

--- index.js
+++ index.js
@@ -374,10 +374,10 @@ async function run() {
         // ! need to delete
 
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
